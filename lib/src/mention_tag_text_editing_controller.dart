e736cb1bac0290632668cc63391da3b19e93ddea
Daham Akalanka <66298072+Daham2000@users.noreply.github.com>
2025-04-04 10:10:12 +0530

    Update mention_tag_text_editing_controller.dart

--- lib/src/mention_tag_text_editing_controller.dart
+++ lib/src/mention_tag_text_editing_controller.dart
@@ -337,7 +337,10 @@ class MentionTagTextEditingController extends TextEditingController {
     );
   }
 
-  bool isURl(String url){
-return  RegExp(r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
-    .hasMatch(url);  }
+  bool isURL(String url) {
+  return RegExp(
+    r'^(https?:\/\/)?(www\.)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,6}(:[0-9]{1,5})?(\/\S*)?$',
+    caseSensitive: false,
+  ).hasMatch(url);
+}
 }
