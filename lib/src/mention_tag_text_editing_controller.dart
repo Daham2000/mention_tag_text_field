3472b2f28d0bd2f634447b8de8bddc7240d4457e
Daham Akalanka <66298072+Daham2000@users.noreply.github.com>
2025-04-04 12:23:31 +0530

    Update mention_tag_text_editing_controller.dart

--- lib/src/mention_tag_text_editing_controller.dart
+++ lib/src/mention_tag_text_editing_controller.dart
@@ -393,10 +393,12 @@ class MentionTagTextEditingController extends TextEditingController {
     }
 
 
-    bool isURl(String url) {
-      return RegExp(
-          r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
-          .hasMatch(url);
-    }
+  
+  }
+
+  bool isURl(String url) {
+    return RegExp(
+        r'^((?:.|\n)*?)((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)([-A-Z0-9.]+)(/[-A-Z0-9+&@#/%=~_|!:,.;]*)?(\?[A-Z0-9+&@#/%=~_|!:‌​,.;]*)?)')
+        .hasMatch(url);
   }
 }
